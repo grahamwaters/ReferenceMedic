ca86426ff090bce96e29632c82abff1391ee6622
Graham Waters <graham.waters.business@gmail.com>
2023-01-17 10:33:28 -0600

    black formatting

--- medic.py
+++ medic.py
@@ -1,7 +1,7 @@
 import os
 import re
-import shutil
 from pathlib import Path
+
 
 def ReferenceMedic(folder):
     """
@@ -12,13 +12,13 @@ def ReferenceMedic(folder):
     for dirpath, dirnames, filenames in os.walk(folder):
         for filename in filenames:
             try:
-                if filename == "reference_medic.py" or '.DS_Store' in filename:
-                    continue # skip this file
+                if filename == "reference_medic.py" or ".DS_Store" in filename:
+                    continue  # skip this file
                 file_path = Path(dirpath) / filename
                 if file_path.suffix == ".ipynb":
                     with open(file_path, "r") as f:
                         content = f.read()
-                    for file_ref in re.findall(r'\"(.+?)\"', content):
+                    for file_ref in re.findall(r"\"(.+?)\"", content):
                         ref_path = Path(file_ref)
                         if ref_path.suffix == ".json":
                             new_path = "../config/" + ref_path.name
@@ -44,7 +44,7 @@ def ReferenceMedic(folder):
                 elif file_path.suffix == ".py":
                     with open(file_path, "r") as f:
                         content = f.read()
-                    for file_ref in re.findall(r'\"(.+?)\"', content):
+                    for file_ref in re.findall(r"\"(.+?)\"", content):
                         ref_path = Path(file_ref)
                         if ref_path.suffix == ".json":
                             new_path = "./config/" + ref_path.name
@@ -70,7 +70,7 @@ def ReferenceMedic(folder):
                 elif file_path.suffix == ".html":
                     with open(file_path, "r") as f:
                         content = f.read()
-                    for file_ref in re.findall(r'\"(.+?)\"', content):
+                    for file_ref in re.findall(r"\"(.+?)\"", content):
                         ref_path = Path(file_ref)
                         if ref_path.suffix == ".json":
                             new_path = "./config/" + ref_path.name
@@ -96,7 +96,7 @@ def ReferenceMedic(folder):
                 elif file_path.suffix == ".css":
                     with open(file_path, "r") as f:
                         content = f.read()
-                    for file_ref in re.findall(r'\"(.+?)\"', content):
+                    for file_ref in re.findall(r"\"(.+?)\"", content):
                         ref_path = Path(file_ref)
                         if ref_path.suffix == ".json":
                             new_path = "./config/" + ref_path.name
@@ -127,12 +127,14 @@ def ReferenceMedic(folder):
                     pass
                 else:
                     print(f"Invalid file type: {file_path.suffix}")
-                    #raise ValueError(f"Invalid file type: {file_path.suffix}")
+                    # raise ValueError(f"Invalid file type: {file_path.suffix}")
             except Exception as e:
                 print(f"Error: {e}")
 
+
 def main():
-    ReferenceMedic("./") # Path to the folder containing the files to be fixed
+    ReferenceMedic("./")  # Path to the folder containing the files to be fixed
+
 
 if __name__ == "__main__":
-    main() # Run the main function
+    main()  # Run the main function
